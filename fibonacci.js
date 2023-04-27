146c612223d4295a3be2ea95f116a33e0a55185a
gedadev <gjbg1234@gmail.com>
2023-04-26 23:42:30 -0600

    fibonacci recursive function
    evaluate 2 base case scenarios, if the input is 0 return the array with the first element
    if the input is 1 return the fist and second element
    else create a new array calling the self function with the input decreased by 1, until it reaches the base case escenario
    when the 1 is reached, the array will be created and then proceed to sum the previous 2 elements for each call and push the result into the array
    when the final call is resolved, return the array

--- fibonacci.js
+++ fibonacci.js
@@ -10,4 +10,17 @@ function fibs(n) {
     return arr;
 }
 
-console.log(fibs(10))
+function fibsRec(n) {
+    if (n < 1) {
+        return [0];
+    } else if (n === 1) {
+        return [0, 1];
+    } else {
+        const arr = fibsRec(n - 1);
+        arr.push(arr[arr.length - 1] + arr[arr.length - 2]);
+        return arr;
+    }
+}
+
+console.log(fibs(8));
+console.log(fibsRec(8));
