8739c2ba7d83695616187714d5a3e9df980c251a
gedadev <gjbg1234@gmail.com>
2023-04-26 22:44:13 -0600

    fibonacci function
    create an array with the first element [0]
    create a loop with the n as a condition
    if the index is 0 (first loop) push the 1 into the array
    for the next loops push the sum of the current array index and the previous one
    return the array

--- fibonacci.js
+++ fibonacci.js
@@ -1,13 +1,13 @@
 function fibs(n) {
     let arr = [0];
-    for (let i = 1; i < n; i++) {
-        if (i == 1) {
+    for (let i = 0; i < n; i++) {
+        if (i == 0) {
             arr.push(1);
         } else {
-            arr.push(arr[i - 2] + arr[i - 1]);
+            arr.push(arr[i] + arr[i - 1]);
         }
     }
     return arr;
 }
 
-console.log(fibs(3))
+console.log(fibs(10))
