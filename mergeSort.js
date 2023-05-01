7170d56dbc1f22cf4aef97d984ba36bcfc1f70c4
gedadev <gjbg1234@gmail.com>
2023-05-01 16:36:01 -0600

    Merge the parts
    Reset the original array
    Create a while loop when both arrays lenght is greater than 0
    Compare the first element of each array and push the lower number shifting the element from the array so the lenght can reach 0 and exit the loop
    Return the original array concatenating the array part that didn't reach the 0 length

--- mergeSort.js
+++ mergeSort.js
@@ -4,5 +4,20 @@ function mergeSort(arr) {
     } else {
         const left = mergeSort(arr.slice(0, arr.length/2));
         const right = mergeSort(arr.slice(arr.length/2, arr.length));
+        
+        arr = [];
+        while(left.length > 0 && right.length > 0){
+            left[0] < right[0] ? arr.push(left.shift()) : arr.push(right.shift());
+        }
+        return arr.concat(left.length > 0 ? left : right);
     }
 }
+
+
+let arr = [4,8,6,2,1,7,5,3];
+let arr2 = [9,8,7,6,5,4,3,2,1]
+let arr3 = [2,6,8,1,3,5,7,4]
+
+console.log(mergeSort(arr))
+console.log(mergeSort(arr2))
+console.log(mergeSort(arr3))
