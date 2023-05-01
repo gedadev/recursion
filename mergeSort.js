6fdfc5a19f5a384f2fc2e835a7e9ad5cb7bb0732
gedadev <gjbg1234@gmail.com>
2023-05-01 16:24:25 -0600

    Slice the array in 2
    Create a constant for the left part of the array, which is the recursion call with the first half of the array
    Create a new constant for the right part, calling the same function with the last half of the array
    The function will return when the array lenght is 1

--- mergeSort.js
+++ mergeSort.js
@@ -2,6 +2,7 @@ function mergeSort(arr) {
     if (arr.length < 2) {
         return arr;
     } else {
-
+        const left = mergeSort(arr.slice(0, arr.length/2));
+        const right = mergeSort(arr.slice(arr.length/2, arr.length));
     }
 }
